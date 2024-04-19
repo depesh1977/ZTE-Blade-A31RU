15d770c0a2eae381623f982ddb92941e110704ec
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2024-04-19 21:41:02 +0300

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_zte.mk
+
+COMMON_LUNCH_CHOICES := twrp_zte-eng
---
+++ twrp_zte.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from BQru_5745L device
+$(call inherit-product, device/zte/zte/device.mk)
+
+PRODUCT_DEVICE := zte
+PRODUCT_NAME := twrp_zte
+PRODUCT_BRAND := zte
+PRODUCT_MODEL := ZTE Blade A31RU
+PRODUCT_MANUFACTURER := zte
